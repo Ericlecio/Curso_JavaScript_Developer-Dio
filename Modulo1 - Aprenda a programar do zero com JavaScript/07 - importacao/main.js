122c0c2046e1d2b8102b189ce1ee9bb0d4df843d
Ericlecio <ericleciojr14@gmail.com>
2023-02-25 17:14:33 -0300

    Atualização de codigo

--- Modulo1 - Aprenda a programar do zero com JavaScript/07 - importacao/main.js
+++ Modulo1 - Aprenda a programar do zero com JavaScript/07 - importacao/main.js
@@ -1,2 +1,17 @@
-const funcoes = require ('./funcoes-auxiliares');
-console.log(funcoes.gets());    
+const { gets, print } = require('./funcoes-auxiliares');
+
+
+let maiorValorEncontrado = 0;
+
+for (let i = 0; i < 5; i++) {
+    const numeroSorteado = gets();
+    if (numeroSorteado > maiorValorEncontrado) {
+        maiorValorEncontrado = numeroSorteado;
+    }
+}
+print(maiorValorEncontrado);
+
+
+
+
+

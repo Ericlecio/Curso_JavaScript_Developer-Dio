a2af2b3464dc17b4a6e4b8dc56aaf625e4b1dcb8
Ericlecio <ericleciojr14@gmail.com>
2023-03-06 10:45:25 -0300

    Atualizando cores dos pokemons

--- Modulo2 - JavaScript Web/Pokedex/assets/js/poke-api.js
+++ Modulo2 - JavaScript Web/Pokedex/assets/js/poke-api.js
@@ -2,11 +2,29 @@
 
 const pokeapi = {}
 
-pokeapi.getPokemonsDetails = (pokemon) => {
-    return fetch(pokemon.url).then((reaponse) => reaponse.json())
+function convertPokeApiDetailsToPokemon(pokeDetails) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetails.order
+    pokemon.name = pokeDetails.name
+
+    const types = pokeDetails.types.map((typeSlot) => typeSlot.type.name)
+    const {type} = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetails.sprites.other.dream_world.front_default
+
+    return pokemon
 }
 
-pokeapi.getPokemons = (offset = 0, limit = 10) => {
+pokeapi.getPokemonsDetails = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((reaponse) => reaponse.json())
+        .then(convertPokeApiDetailsToPokemon)
+}
+
+pokeapi.getPokemons = (offset = 0, limit = 50) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response) => response.json())
